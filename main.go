26a4865b8eed9a4484653ecfbfadc446cc4ed8b7
Guillex387 <guillesiglez387@gmail.com>
2022-11-30 18:05:02 +0000

    Resolved all bugs

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 )
 
 type Client struct {
@@ -25,6 +26,17 @@ func GetLocalIp() string {
 	return localAddr.IP.String()
 }
 
+func FormatString(str string) string {
+	result := ""
+	for _, char := range str {
+		if char != '\n' {
+			result += string(char)
+		}
+	}
+	result += "\n"
+	return result
+}
+
 func CheckError(err error) {
 	if err != nil {
 		log.Fatal(err)
@@ -33,31 +45,106 @@ func CheckError(err error) {
 
 // Clients Management
 
+/*
+ A function for add a user to the chat
+*/
 func AddUser(name string, conn net.Conn, userArr *[]Client) {
 	*userArr = append(*userArr, Client{Name: name, Conection: conn})
+	userIndex := len(*userArr) - 1
+	go UserListener(userIndex, userArr)
+	messageToSend := FormatString(name + " join to the chat")
+	ReplyAll(messageToSend, userArr, name)
 }
 
-func FindIndex(user Client, userArr *[]Client) int {
+/*
+ A function for find a user in the user array
+*/
+func FindIndex(userName string, userArr *[]Client) int {
 	for i := 0; i < len(*userArr); i++ {
-		if (*userArr)[i].Name == user.Name {
+		if (*userArr)[i].Name == userName {
 			return i
-		} 
+		}
 	}
 	return -1
 }
 
+/*
+ A function for delete a user in the server
+*/
 func DeleteUser(user Client, userArr *[]Client) {
-	findIndex := FindIndex(user, userArr)
+	findIndex := FindIndex(user.Name, userArr)
 	if findIndex == -1 {
 		return
 	}
 	slice1 := (*userArr)[0:findIndex]
 	slice2 := (*userArr)[(findIndex + 1):]
 	*userArr = append(slice1, slice2...)
+	user.Conection.Close()
+	ReplyAll(FormatString(user.Name + " closed connection"), userArr, "")
+}
+
+/*
+ A function that reads the actions of a user
+*/
+func UserListener(userIndex int, userArr *[]Client) {
+	user := (*userArr)[userIndex]
+	for {
+		message, _ := bufio.NewReader(user.Conection).ReadString('\n')
+		if message == "END\n" {
+			DeleteUser(user, userArr)
+			return
+		}
+		messageToSend := FormatString(user.Name + ": " + message)
+		ReplyAll(messageToSend, userArr, user.Name)
+		time.Sleep(time.Second / 2)
+	}
+}
+
+/*
+ A function that listen new client connections
+*/
+func AcceptClients(listener net.Listener, userArr *[]Client, exit chan int) {
+	for {
+		conn, err := listener.Accept()
+		CheckError(err)
+		name, _ := bufio.NewReader(conn).ReadString('\n')
+		if FindIndex(name, userArr) != -1 {
+			conn.Write([]byte("Error, the name already exists\n"))
+			conn.Close()
+			continue
+		}
+		AddUser(name, conn, userArr)
+		time.Sleep(time.Second / 2)
+	}
+}
+
+/*
+ A function for send a messages to all user saved in the user array
+*/
+func ReplyAll(message string, userArr *[]Client, exception string) {
+	fmt.Print(message)
+	for  _, compi := range *userArr {
+		if compi.Name != exception {
+			compi.Conection.Write([]byte(message))
+		}
+	}
+}
+
+/*
+ A function that constantly read the messages from a connection
+*/
+func MessagesListener(conn net.Conn) {
+	for {
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print(message)
+	}
 }
 
 // Modes
 
+/*
+ A function for create a client for the chat
+*/
 func ClientMode() {
 	var ip string
 	var port string
@@ -68,15 +155,18 @@ func ClientMode() {
 	con, err := net.Dial("tcp4", ip + ":" + port)
 	reader := bufio.NewReader(os.Stdin)
 	CheckError(err)
+	firstIteration := true
 	for {
-		fmt.Print(">> ")
+		if firstIteration {
+			fmt.Print("Insert your nick name: ")
+			go MessagesListener(con)
+			firstIteration = false
+		}
 		msg, _ := reader.ReadString('\n')
-		_, err = con.Write([]byte(msg))
-		CheckError(err)
-		reply := make([]byte, 1024)
-		_, err = con.Read(reply)
-		CheckError(err)
-		fmt.Println(string(reply))
+		if msg == "\n" {
+			continue
+		}
+		con.Write([]byte(msg))
 		if msg == "END\n" {
 			break
 		}
@@ -84,28 +174,31 @@ func ClientMode() {
 	con.Close()
 }
 
+/*
+ A function that creates the chat host
+*/
 func ServerMode(port string) {
 	fmt.Printf("Server IP: %s\n", GetLocalIp())
 	fmt.Printf("Server PORT: %s\n", port)
 	listener, err := net.Listen("tcp", ":" + port)
 	CheckError(err)
-	conn, err := listener.Accept()
-	CheckError(err)
-	for {
-    message, _ := bufio.NewReader(conn).ReadString('\n')
-		if message == "" {
-			continue
-		}
-		conn.Write([]byte("Received"))
-    fmt.Print("\nMessage Received: ", string(message))
-  }
+	var userArr []Client
+	exitChannel := make(chan int, 1)
+	AcceptClients(listener, &userArr, exitChannel)
 }
 
 func main() {
-	// mode := os.Args[1]
-	// if mode == "server" {
-	// 	ServerMode("12345")
-	// } else {
-	// 	ClientMode()
-	// }
+	if len(os.Args) < 2 {
+		fmt.Println("Error: You don't put parameters")
+		os.Exit(2)
+	}
+	mode := os.Args[1]
+	if mode == "server" {
+		if len(os.Args) != 3 {
+			return
+		}
+		ServerMode(os.Args[2])
+	} else {
+		ClientMode()
+	}
 }
