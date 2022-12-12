b20a149691ee582f17974a135171580024c194a7
Guillex387 <guillesiglez387@gmail.com>
2022-12-12 19:41:24 +0000

    Refactor some functions

---
+++ src/lib/utils.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"net"
+)
+
+// Returns the local ip string
+func GetLocalIp() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return ""
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP.String()
+}
