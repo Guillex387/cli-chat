cf18c67e5e09a5d8b287487483268ec17337a62e
Guillex387 <guillesiglez387@gmail.com>
2023-01-12 14:43:16 +0000

    Defined the errors

---
+++ chat/errors.go
@@ -0,0 +1,19 @@
+package chat
+
+type OpenConnectionError struct {}
+
+func (e *OpenConnectionError) Error() string {
+	return "Error opening the connection"
+}
+
+type OpenServerError struct {}
+
+func (e *OpenServerError) Error() string {
+	return "Error creating the server"
+}
+
+type ConnectionIOError struct {}
+
+func (e *ConnectionIOError) Error() string {
+	return "Error reading or writing in the connection"
+}
