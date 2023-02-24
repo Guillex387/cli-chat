f03e29ef420b24cee41d9ea233c4e8c614b5039d
guillex387 <guillesiglez387@gmail.com>
2023-02-24 09:32:45 +0000

    Some improvements in the parser

--- ui/parser.go
+++ ui/parser.go
@@ -1,25 +1,63 @@
 package ui
 
-import "strings"
-
 // Parse a instruction from the
 // user input
 type InputInstruction struct {
   Id string
-  Body string
+  Body []string
 }
 
 // Parse instruction from user input 
 func ParseInstruction(input string) InputInstruction {
+  // Check if is a null input and returns a void instruction
+  if len(input) == 0 {
+    return InputInstruction{
+      Id: "",
+      Body: []string{""},
+    }
+  }
+  // Checks if input not contain a instruction id
+  // and returns the message id ("") and the entire
+  // input as the body
   if input[0] != '/' {
     return InputInstruction{
       Id: "",
-      Body: input,
+      Body: []string{input},
     }
   }
-  splits := strings.Split(input[1:], " ")
+  input += " "
+  splits := make([]string, 0)
+  openSemiCol := false
+  token := ""
+  // This loops checks the letters and
+  // separates the input in tokens
+  for _, letter := range input[1:] {
+    if letter == '"' {
+      if openSemiCol {
+        splits = append(splits, token)
+        openSemiCol = false
+        token = ""
+        continue
+      }
+      openSemiCol = true
+      token = ""
+      continue
+    }
+    if letter == ' ' && !openSemiCol {
+      if len(token) != 0 {
+        splits = append(splits, token)
+      }
+      token = ""
+      continue
+    }
+    token += string(letter)
+  }
+  // Checks if exists a remain token
+  if len(token) != 0 {
+    splits = append(splits, token)
+  }
   return InputInstruction{
     Id: splits[0],
-    Body: strings.Join(splits[1:], " "),
+    Body: splits[1:],
   }
 }
