fcc03e5d324512a3fec5d26f947479725c203a19
guillex387 <guillesiglez387@gmail.com>
2023-09-10 03:24:01 +0100

    Defined the main

--- main.go
+++ main.go
@@ -1,56 +1,47 @@
 package main
 
-// Example of client for the ui
-
 import (
+	"cli-chat/args"
 	"cli-chat/chat"
 	"cli-chat/ui"
 	"fmt"
+	"os"
 
-	// tea "github.com/charmbracelet/bubbletea"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-type ClientEx struct {
-  SendEvent chat.Event
-}
-
-func NewClient() chat.Client {
-  return &ClientEx{SendEvent: chat.NewEvent()}
-}
-
-func (c *ClientEx) Event() *chat.Event {
-  return &c.SendEvent
-}
-
-func (c *ClientEx) SendInstruction(instruction chat.Instruction) error {
-  c.SendEvent.Trigger(instruction)
-  return nil
-}
-
-func (c *ClientEx) Close() {
-  c.SendEvent.Clear()
+func GetClient(ctx args.CliArgs) chat.Client {
+  if ctx.Mode == "client" {
+    client, err := chat.OpenConnection(ctx.Ip, ctx.Port, ctx.Name)
+    if err != nil {
+      fmt.Println(err.Error())
+      os.Exit(1)
+    }
+    return client
+  }
+  server, err := chat.InitServer(ctx.Port)
+  if err != nil {
+    fmt.Println(err.Error())
+    os.Exit(1)
+  }
+  client := chat.CreateServerClient(server)
+  return client
 }
 
 func main() {
-  mode := ""
-  ip := ""
-  port := ""
-  ui.InitArgs(&mode, &ip, &port)
-  ui.Parse()
-  fmt.Printf("%s -> %s:%s\n", mode, ip, port)
-  // client := NewClient()
-  // style := ui.NewStyle("#70EBFF", "#F00057", "#70EBFF")
-  // data := ui.NewModelData(style)
-  // program := tea.NewProgram(ui.InitModel(&client, &data))
-  // client.Event().On("", func(this chat.EventListener, instruction chat.Instruction) {
-  //   data.AddMessage("You", string(instruction.Args[1]))
-  // })
-  // client.Event().On("error", func(this chat.EventListener, instruction chat.Instruction) {
-  //   data.AddError(string(instruction.Args[0]))
-  // })
-  // client.Event().On("end", func(this chat.EventListener, instruction chat.Instruction) {
-  //   program.Quit()
-  // })
-  // program.Run()
-  // TODO: define the main with an argument parser
+  ctx := args.CliArgs{
+    Mode: "",
+    Ip: "",
+    Port: "",
+    Name: "",
+  }
+  args.InitArgs(&ctx)
+  args.Parse()
+  style := ui.NewStyle("#70EBFF", "#F00057", "#70EBFF")
+  data := ui.NewModelData(style)
+  client := GetClient(ctx)
+  go client.Listen()
+  program := tea.NewProgram(ui.InitModel(&client, &data))
+  // Run the terminal ui
+  program.Run()
 }
