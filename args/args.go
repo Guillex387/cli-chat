561691d7f66efe1c181e9be70b335129ebb3b60e
guillex387 <guillesiglez387@gmail.com>
2023-09-15 18:31:13 +0100

    Help message improvement

--- args/args.go
+++ args/args.go
@@ -2,6 +2,16 @@ package args
 
 import "flag"
 
+const DESCRIPTION = "Terminal based chat with multiple functionality" +
+                    " which has its own data transfer protocol.\n"
+const MODE_HELP = "Choose the execution mode of the program, \"client\", \"server\""
+
+const PORT_HELP = "The port to listen or connect to a server"
+
+const IP_HELP = "IP direction to connect to a host (client only)"
+
+const NAME_HELP = "The display name in the chat (client only)"
+
 type CliArgs struct {
   Mode string
   Ip string
@@ -10,14 +20,23 @@ type CliArgs struct {
 }
 
 func InitArgs(cli *CliArgs) {
-  flag.StringVar(&cli.Mode, "mode", "client", "Value: \"client\", \"server\". Choose the execution mode of the program")
-  flag.StringVar(&cli.Port, "port", "8000", "The port to listen or connect to a server")
-  flag.StringVar(&cli.Ip, "ip", "", "IP direction to connect to a host (client only)")
-  flag.StringVar(&cli.Name, "name", "unknow", "The display name in the chat (client only)")
+  flag.StringVar(&cli.Mode, "mode", "client", MODE_HELP)
+  flag.StringVar(&cli.Port, "port", "8000", PORT_HELP)
+  flag.StringVar(&cli.Ip, "ip", "127.0.0.1", IP_HELP)
+  flag.StringVar(&cli.Name, "name", "unknown", NAME_HELP)
+
+  flag.Usage = func() {
+    println(DESCRIPTION)
+
+    println("Options:")
+    flag.PrintDefaults()
+  }
 }
 
 func Parse() {
-  if !flag.Parsed() {
-    flag.Parse()
+  if flag.Parsed() {
+    return
   }
+
+  flag.Parse()
 }
